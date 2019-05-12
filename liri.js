12ab0aa1fca710f374fa66e97dd25427793576a8
fyeh0 <fangiyeh@gmail.com>
2019-05-11 22:55:46 -0400

    test movie output

--- liri.js
+++ liri.js
@@ -1,6 +1,58 @@
 require("dotenv").config();
-var keys = require("./keys.js");
+var inquirer = require("inquirer");
+var axios = require("axios");
 
-var spotify = new Spotify(keys.spotify);
+var keys = require("./keys.js");
+// var omdb = require('omdb');
+
+// var spotify = new Spotify(keys.spotify);
 
 const userInput = process.argv[2];
+
+var queryUrl =
+  "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+
+axios.get(queryUrl).then(function(response) {
+  console.log(response.data.Title + " was released in " + response.data.Year);
+});
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Who are you?"
+    },
+    {
+      type: "list",
+      name: "lookingFor",
+      message: "What are you doing here?",
+      choices: [
+        "I'm looking for a movie",
+        "I'm looking for a song",
+        "I'm looking for an artist"
+      ]
+    }
+  ])
+  .then(function(userInput) {
+    if (userInput.choices === "I'm looking for a movie") {
+      var queryUrl =
+        "http://www.omdbapi.com/?t=" +
+        userInput +
+        "&y=&plot=short&apikey=trilogy";
+
+      axios
+        .get(queryUrl)
+        .then(function(response) {
+          console.log(
+            response.data.Title + " was released in " + response.data.Year
+          );
+        })
+        .catch(function(error) {
+          if (error.response) {
+            console.log(error.response);
+            console.log("Sorry, I didn't catch that.");
+          }
+        });
+    }
+  });
