32454fab759d32e1982ba90f91d6c6393709fb33
fyeh0 <fangiyeh@gmail.com>
2019-05-12 19:14:19 -0400

    partial function logic, switch case

--- liri.js
+++ liri.js
@@ -1,58 +1,87 @@
 require("dotenv").config();
+var fs = require("fs");
 var inquirer = require("inquirer");
 var axios = require("axios");
-
 var keys = require("./keys.js");
-// var omdb = require('omdb');
+var omdb = require("omdb");
+var moment = require("moment");
+moment().format();
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
 
-// var spotify = new Spotify(keys.spotify);
-
+const command = process.argv[2];
 const userInput = process.argv[2];
 
-var queryUrl =
-  "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+switch (command) {
+    case movieThis():
+    console.log(response);
+    break;
+    default:
+    console.log("default");
+}
 
-axios.get(queryUrl).then(function(response) {
-  console.log(response.data.Title + " was released in " + response.data.Year);
-});
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "Who are you?"
-    },
-    {
-      type: "list",
-      name: "lookingFor",
-      message: "What are you doing here?",
-      choices: [
-        "I'm looking for a movie",
-        "I'm looking for a song",
-        "I'm looking for an artist"
-      ]
-    }
-  ])
-  .then(function(userInput) {
-    if (userInput.choices === "I'm looking for a movie") {
-      var queryUrl =
-        "http://www.omdbapi.com/?t=" +
-        userInput +
-        "&y=&plot=short&apikey=trilogy";
+function concertThis(userInput) {
+    
+}
 
-      axios
-        .get(queryUrl)
-        .then(function(response) {
-          console.log(
-            response.data.Title + " was released in " + response.data.Year
-          );
-        })
-        .catch(function(error) {
-          if (error.response) {
-            console.log(error.response);
-            console.log("Sorry, I didn't catch that.");
-          }
-        });
-    }
-  });
+function movieThis() {
+    var queryUrl =
+      "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+    
+    axios.get(queryUrl).then(function(response) {
+        console.log(response.data);
+    });
+    // fs.readFile("random.txt", "utf8", function(err, data) {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+    // });
+}
+
+function spotifyThis() {
+    spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        
+        console.log(data); 
+    });
+}
+
+function doThis() {
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        console.log(data);
+    })
+}
+
+
+
+    // fs.writeFile("random.txt", userInput, err); {
+    // if (err) {
+    //     return console.log(response.data.err);
+    //     } else {
+    //     console.log(response.data);
+    //     console.log(response.data.Title);
+    //     console.log(response.data.Year);
+    //     console.log(response.data.Rated);
+    //     console.log(response.data.Rating);
+    //     console.log(response.data.Plot);
+    //     console.log(response.data.Actors);
+    // }  
+    // } 
+    
+    // inquirer
+    //     .prompt([
+    //         {
+    //             type: "input",
+    //             message: "Who are you?",
+    //             name: "name"
+    //         },
+    //         {
+    //             type: "list",
+    //             message: "What would you like to do?",
+    //             choices: ["Concert this", "spotify this song", "movie this", "do what it says"],
+    //             name: "command"
+    //         }
+    //     ])
