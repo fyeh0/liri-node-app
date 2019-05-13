f73c2e9bc472062f2b3b23d22da1997ab2a2c795
fyeh0 <fangiyeh@gmail.com>
2019-05-12 20:41:35 -0400

    bandsintown npm

--- liri.js
+++ liri.js
@@ -13,75 +13,89 @@ const command = process.argv[2];
 const userInput = process.argv[2];
 
 switch (command) {
-    case movieThis():
+  case movieThis():
     console.log(response);
     break;
-    default:
+  case spotifyThis():
+    console.log(response);
+    break;
+  default:
     console.log("default");
 }
 
-
-function concertThis(userInput) {
-    
-}
+function concertThis(userInput) {}
 
 function movieThis() {
-    var queryUrl =
-      "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
-    
-    axios.get(queryUrl).then(function(response) {
-        console.log(response.data);
+  var queryUrl =
+    "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+
+  axios
+    .get(queryUrl)
+    .then(function(response) {
+      console.log(response.data);
+    })
+    .catch(function(err) {
+      if (err === "") {
+        axios
+          .get(
+            "http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=trilogy"
+          )
+          .then(function(response) {
+            console.log("The movie's rating is: " + response.data.imdbRating);
+          });
+      }
     });
-    // fs.readFile("random.txt", "utf8", function(err, data) {
-    //     if (err) {
-    //         return console.log(err);
-    //     }
-    // });
+  // fs.readFile("random.txt", "utf8", function(err, data) {
+  //     if (err) {
+  //         return console.log(err);
+  //     }
+  // });
 }
 
 function spotifyThis() {
-    spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        
-        console.log(data); 
-    });
+  spotify.search({ type: "track", query: "All the Small Things" }, function(
+    err,
+    data
+  ) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+
+    console.log(data);
+  });
 }
 
 function doThis() {
-    fs.readFile("random.txt", "utf8", function(err, data) {
-        console.log(data);
-    })
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    console.log(data);
+  });
 }
 
+// fs.writeFile("random.txt", userInput, err); {
+// if (err) {
+//     return console.log(response.data.err);
+//     } else {
+//     console.log(response.data);
+//     console.log(response.data.Title);
+//     console.log(response.data.Year);
+//     console.log(response.data.Rated);
+//     console.log(response.data.Rating);
+//     console.log(response.data.Plot);
+//     console.log(response.data.Actors);
+// }
+// }
 
-
-    // fs.writeFile("random.txt", userInput, err); {
-    // if (err) {
-    //     return console.log(response.data.err);
-    //     } else {
-    //     console.log(response.data);
-    //     console.log(response.data.Title);
-    //     console.log(response.data.Year);
-    //     console.log(response.data.Rated);
-    //     console.log(response.data.Rating);
-    //     console.log(response.data.Plot);
-    //     console.log(response.data.Actors);
-    // }  
-    // } 
-    
-    // inquirer
-    //     .prompt([
-    //         {
-    //             type: "input",
-    //             message: "Who are you?",
-    //             name: "name"
-    //         },
-    //         {
-    //             type: "list",
-    //             message: "What would you like to do?",
-    //             choices: ["Concert this", "spotify this song", "movie this", "do what it says"],
-    //             name: "command"
-    //         }
-    //     ])
+// inquirer
+//     .prompt([
+//         {
+//             type: "input",
+//             message: "Who are you?",
+//             name: "name"
+//         },
+//         {
+//             type: "list",
+//             message: "What would you like to do?",
+//             choices: ["Concert this", "spotify this song", "movie this", "do what it says"],
+//             name: "command"
+//         }
+//     ])
