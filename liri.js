33c929c24ba9f5331c45bc8aac923b0e67e37ff3
fyeh0 <fangiyeh@gmail.com>
2019-05-14 22:53:22 -0400

    log.txt file added

--- liri.js
+++ liri.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 var fs = require("fs");
 var mdash = require("mdash");
-var inquirer = require("inquirer");
 var axios = require("axios");
 var keys = require("./keys.js");
 var omdb = require("omdb");
@@ -9,24 +8,24 @@ var moment = require("moment");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var bandsintown = require("bandsintown"); //("codingbootcamp");
+var inquirer = require("inquirer");
 
-const cmd = process.argv[2];
+const command = process.argv[2];
 const userInput = process.argv.slice(3).join(" ");
 
 // Liri logic
-function runLiri (command, userIn) {
+function runLiri (command, userInput) {
   switch (command) {
     case "movie-this":
-      movieThis(userIn);
+      movieThis(userInput);
       break;
     case "spotify-this": 
-      spotifyThis(userIn);
+      spotifyThis(userInput);
       break;
     case "concert-this":
-      concertThis(userIn);
+      concertThis(userInput);
       break;
     case "do-this":
-    //   console.log(userIn);
       doThis();
       break;
     default:
@@ -54,13 +53,29 @@ function concertThis(userInput) {
     .then(function(response) {
       console.log(`
         ·.¸¸.·♩♪♫  I found this for you!  ♫♪♩·.¸¸.·
+
         Venue: ${response.data[0].venue.name}
         Location: ${response
           .data[0].venue.city}, ${response.data[0].venue.region}
-        Date: ${moment(response.data[0].datetime).format("llll")}`);
+        Date: ${moment(response.data[0].datetime).format("llll")}
+
+        ==========================================
+            I'll add this to your search log!
+        `);
+        fs.appendFileSync("log.txt", `
+        Search item: ${userInput}
+        -------------------------
+        Venue: ${response.data[0].venue.name}
+        Location: ${response
+          .data[0].venue.city}, ${response.data[0].venue.region}
+        Date: ${moment(response.data[0].datetime).format("llll")}
+        `, "utf8")
     })
     .catch(function(err) {
       if (err) {
+          fs.appendFileSync("log.txt", `
+    I'm sorry, I didn't find anything for ${userInput}"
+          `, "utf8");
         return console.log("Sorry, I didn't catch that.");
       }
     });
@@ -78,6 +93,7 @@ function movieThis(userInput) {
     .then(function(response) {
         console.log(`
     ════════════ I found this for you! ════════════
+
         Title: ${response.data.Title}
         Year: ${response.data.Year}
         IMDB Rating: ${response.data.imdbRating}
@@ -85,18 +101,28 @@ function movieThis(userInput) {
         Country: ${response.data.Country}
         Language: ${response.data.Language}
         Plot: ${response.data.Plot}
-        Cast: ${response.data.Actors}`);
+        Cast: ${response.data.Actors}
 
-    //   console.log("Title: " + response.data.Title);
-    //   console.log("Year: " + response.data.Year);
-    //   console.log("IMDB Rating: " + response.data.imdbRating);
-    //   console.log("Rotton Tomatoes Rating: " + response.data.Ratings[1]);
-    //   console.log("Country: " + response.data.Country);
-    //   console.log("Language: " + response.data.Language);
-    //   console.log("Plot: " + response.data.Plot);
-    //   console.log("Actors: " + response.data.Actors);
+     ==========================================
+        I'll add this to your search log!
+        `);
+        fs.appendFileSync("log.txt", `
+        Search item: ${userInput}
+        -------------------------
+        Title: ${response.data.Title}
+        Year: ${response.data.Year}
+        IMDB Rating: ${response.data.imdbRating}
+        Rotten Tomatoes Rating: ${response.data.Ratings[1]}
+        Country: ${response.data.Country}
+        Language: ${response.data.Language}
+        Plot: ${response.data.Plot}
+        Cast: ${response.data.Actors}
+        `, "utf8");
     })
     .catch(function(err) {
+        fs.appendFileSync("log.txt", `
+    I'm sorry, I didn't find anything for ${userInput}"
+          `, "utf8");
       console.log(err.response);
     });
 }
@@ -107,49 +133,59 @@ function spotifyThis(userInput) {
   }
   spotify.search({ type: "track", query: userInput }, function(err, response) {
     if (err) {
-      console.log("Sorry, I didn't catch that. " + err);
+        fs.appendFileSync("log.txt", `
+    I'm sorry, I didn't find anything for ${userInput}"
+          `, "utf8");
+      console.log("Sorry, I didn't catch that.");
     } else {
       console.log(`
           ·.¸¸.·♩♪♫  I found this for you!  ♫♪♩·.¸¸.·
+
         Song: ${response.tracks.items[0].name}
         Artist: ${response.tracks.items[0].artists[0].name}
         Album: ${response.tracks.items[0].album.name}
-        Preview link: ${response.tracks.items[0].href}`);
+        Preview link: ${response.tracks.items[0].href}
+        
+         ==========================================
+            I'll add this to your search log!
+            `);
+        fs.appendFileSync("log.txt", `
+        Search item: ${userInput}
+        -------------------------
+        Song: ${response.tracks.items[0].name}
+        Artist: ${response.tracks.items[0].artists[0].name}
+        Album: ${response.tracks.items[0].album.name}
+        Preview link: ${response.tracks.items[0].href}
+        `, "utf8");
     }
   })
-  return anotherSearch();
+//   return anotherSearch();
 }
 
 function doThis() {
   fs.readFile("random.txt", "utf8", function(err, data) {
       var dataArr = data.split(",");
-      console.log(dataArr[1].replace('"','').replace('"',''));
-      runLiri(dataArr[0].replace('-song',''), dataArr[1].replace('"','').replace('"',''));
-
-      // if () {
-
-      // }
-    // console.log("do-this data= ", dataArr);
+      runLiri(dataArr[0].replace('-song',''), dataArr[1].replace(/['"]+/g, ''));
   });
 }
 
-function anotherSearch() {
-    inquirer
-        .prompt([
-            {
-                type: "confirm",
-                message: "Are you still looking for something?",
-                name: "anotherSearch",
-            }
-        ]).then(function(response) {
-            if (response.anotherSearch) {
-                console.log("Okay!");
-                runLiri();
-            } else {
-                console.log("¯\_(ツ)_/¯");
-                console.log("Okay, bye!");
-            }
-        })
-}
+// function anotherSearch() {
+//     inquirer
+//         .prompt([
+//             {
+//                 type: "confirm",
+//                 message: "Are you still looking for something?",
+//                 name: "anotherSearch",
+//             }
+//         ]).then(function(response) {
+//             if (response.anotherSearch) {
+//                 console.log("Okay!");
+//                 runLiri();
+//             } else {
+//                 console.log("¯\_(ツ)_/¯");
+//                 console.log("Okay, bye!");
+//             }
+//         })
+// }
 
-runLiri(cmd, userInput);
+runLiri(command, userInput);
