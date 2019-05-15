eee99d763e1c7fe62176e65f0934ebb87b586219
fyeh0 <fangiyeh@gmail.com>
2019-05-14 21:20:56 -0400

    error cases, default cases

--- liri.js
+++ liri.js
@@ -1,44 +1,46 @@
 require("dotenv").config();
 var fs = require("fs");
 var mdash = require("mdash");
-// var inquirer = require("inquirer");
+var inquirer = require("inquirer");
 var axios = require("axios");
 var keys = require("./keys.js");
 var omdb = require("omdb");
 var moment = require("moment");
-// moment().format();
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var bandsintown = require("bandsintown"); //("codingbootcamp");
 
-const command = process.argv[2];
+const cmd = process.argv[2];
 const userInput = process.argv.slice(3).join(" ");
 
 // Liri logic
-switch (command) {
-  case "movie-this":
-    movieThis(userInput);
-    break;
-  case "spotify-this":
-    spotifyThis(userInput);
-    break;
-  case "concert-this":
-    concertThis(userInput);
-    break;
-  case "do-this":
-    doThis();
-    break;
-  default:
-    console.log(`
-       Hi! I'm Liri!
-    -------------------
-    Please enter one of my following command options!
+function runLiri (command, userIn) {
+  switch (command) {
+    case "movie-this":
+      movieThis(userIn);
+      break;
+    case "spotify-this": 
+      spotifyThis(userIn);
+      break;
+    case "concert-this":
+      concertThis(userIn);
+      break;
+    case "do-this":
+    //   console.log(userIn);
+      doThis();
+      break;
+    default:
+      console.log(`
+            】 Hi! I'm Liri! 【
+        ---------------------------
+Please enter one of my following command options!
     
-    spotify-this "song" : to search for a song
-    concert-this "artist" : to search for a concert
-    movie-this "movie" : to search for a movie
-    -------------------`);
-    break;
+    » spotify-this "song" : to search for a song
+    » concert-this "artist" : to search for a concert
+    » movie-this "movie" : to search for a movie
+        --------------------------`);
+      break;
+  }
 }
 
 function concertThis(userInput) {
@@ -47,14 +49,15 @@ function concertThis(userInput) {
     userInput +
     "/events?app_id=codingbootcamp";
 
-  bandsintown
-    .getArtist(queryUrl)
+  axios
+    .get(queryUrl)
     .then(function(response) {
-        for (let i = 0; i < response.data.length; i++) {
-      console.log(response.data.vanue.name);
-      console.log(response.data.venue.city);
-      console.log("concert stuff here");
-        }
+      console.log(`
+        ·.¸¸.·♩♪♫  I found this for you!  ♫♪♩·.¸¸.·
+        Venue: ${response.data[0].venue.name}
+        Location: ${response
+          .data[0].venue.city}, ${response.data[0].venue.region}
+        Date: ${moment(response.data[0].datetime).format("llll")}`);
     })
     .catch(function(err) {
       if (err) {
@@ -64,57 +67,89 @@ function concertThis(userInput) {
 }
 
 function movieThis(userInput) {
+  if (userInput === "") {
+    userInput = "Mr. Nobody";
+  }
   var queryUrl =
     "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
 
   axios
     .get(queryUrl)
     .then(function(response) {
-      console.log("Title: " + response.data.Title);
-      console.log("Year: " + response.data.Year);
-      console.log("IMDB Rating: " + response.data.imdbRating);
-      console.log("Rotton Tomatoes Rating: " + response.data.Ratings[1]);
-      console.log("Country: " + response.data.Country);
-      console.log("Language: " + response.data.Language);
-      console.log("Plot: " + response.data.Plot);
-      console.log("Actors: " + response.data.Actors);
+        console.log(`
+    ════════════ I found this for you! ════════════
+        Title: ${response.data.Title}
+        Year: ${response.data.Year}
+        IMDB Rating: ${response.data.imdbRating}
+        Rotten Tomatoes Rating: ${response.data.Ratings[1]}
+        Country: ${response.data.Country}
+        Language: ${response.data.Language}
+        Plot: ${response.data.Plot}
+        Cast: ${response.data.Actors}`);
+
+    //   console.log("Title: " + response.data.Title);
+    //   console.log("Year: " + response.data.Year);
+    //   console.log("IMDB Rating: " + response.data.imdbRating);
+    //   console.log("Rotton Tomatoes Rating: " + response.data.Ratings[1]);
+    //   console.log("Country: " + response.data.Country);
+    //   console.log("Language: " + response.data.Language);
+    //   console.log("Plot: " + response.data.Plot);
+    //   console.log("Actors: " + response.data.Actors);
     })
     .catch(function(err) {
-      if (err) {
-        console.log("inside the err");
-        axios
-          .get(
-            "http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=trilogy"
-          )
-          .then(function(response) {
-            console.log(err.response);
-          });
-      }
+      console.log(err.response);
     });
 }
 
 function spotifyThis(userInput) {
-  spotify
-    .search({ type: "track", query: userInput }, function(err, response) {
-        if(err) {
-            console.log("oops" + err);
-        } else {
-            console.log(`
-            ·.¸¸.·♩♪♫ I found this for you! ♫♪♩·.¸¸.·
+  if (userInput === "") {
+    userInput = "The Sign Ace of Base";
+  }
+  spotify.search({ type: "track", query: userInput }, function(err, response) {
+    if (err) {
+      console.log("Sorry, I didn't catch that. " + err);
+    } else {
+      console.log(`
+          ·.¸¸.·♩♪♫  I found this for you!  ♫♪♩·.¸¸.·
         Song: ${response.tracks.items[0].name}
         Artist: ${response.tracks.items[0].artists[0].name}
         Album: ${response.tracks.items[0].album.name}
-        Preview link: ${response.tracks.items[0].href}`)
-            // console.log("Song: " + response.tracks.items[0].name); //song name
-            // console.log("Artist: " + response.tracks.items[0].artists[0].name); // artist name
-            // console.log("Album: " + response.tracks.items[0].album.name); //album name
-            // console.log("Preview link: " + response.tracks.items[0].href); //preview link
-        }
-    })
+        Preview link: ${response.tracks.items[0].href}`);
+    }
+  })
+  return anotherSearch();
 }
 
 function doThis() {
   fs.readFile("random.txt", "utf8", function(err, data) {
-    console.log(data);
+      var dataArr = data.split(",");
+      console.log(dataArr[1].replace('"','').replace('"',''));
+      runLiri(dataArr[0].replace('-song',''), dataArr[1].replace('"','').replace('"',''));
+
+      // if () {
+
+      // }
+    // console.log("do-this data= ", dataArr);
   });
 }
+
+function anotherSearch() {
+    inquirer
+        .prompt([
+            {
+                type: "confirm",
+                message: "Are you still looking for something?",
+                name: "anotherSearch",
+            }
+        ]).then(function(response) {
+            if (response.anotherSearch) {
+                console.log("Okay!");
+                runLiri();
+            } else {
+                console.log("¯\_(ツ)_/¯");
+                console.log("Okay, bye!");
+            }
+        })
+}
+
+runLiri(cmd, userInput);
